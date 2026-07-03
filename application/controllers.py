387efde689a1362bb0636502a8c9a0adb5b0d9fd
anuraag <anuraag.edu@gmail.com>
2026-07-03 17:00:47 +0530

    add couple template pages

--- application/controllers.py
+++ application/controllers.py
@@ -1,4 +1,40 @@
 from flask import Flask, render_template, redirect, request
 from flask import current_app as app
 from .models import * 
-# @app.route('/')
+
+@app.route('/login', methods = ["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        pwd = request.form.get("pwd")
+        this_user = User.query.filter_by(username=username).first()
+        if this_user:
+            if this_user.password == pwd:
+                if this_user.type == "manager":
+                    return redirect("/manager")
+                else:
+                    return render_template("user_dash.html", this_user=this_user)
+            else:
+                return render_template("incorrect_p.html")
+        else:
+            return render_template("not_exist.html")
+
+    return render_template("login.html")
+
+@app.route("/register", methods = ["GET","POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        pwd = request.form.get("pwd")
+        user_name = User.query.filter_by(username = username).first()
+        user_email = User.query.filter_by(email = email).first()
+        if user_name or user_email:
+            return render_template("already.html")
+        else:
+            new_user = User(username = username, email = email, password=pwd)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/login")
+        
+    return render_template("register.html")
